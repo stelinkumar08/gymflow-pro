f7b186ba4863b578281f8b2ddf543c0bb6c6e832
Stelin Kumar <stelinkumar08@gmail.com>
2026-06-10 16:45:39 +0530

    Django Day 4 - nested serializer, plan details in member API

--- backend/gym/serializers.py
+++ backend/gym/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers #importing srealizer from rest framework
-from .models import Member #importing Member class from models.py in gym
+from .models import Member, Plan #importing Member class from models.py in gym
 
+
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = ['name', 'price']
 class MemberSerializer(serializers.ModelSerializer): #creating a class called MemberSerializer. In that bracet importing two of the called models.
+    plan = PlanSerializer()
     class Meta: #creating a second class in Main class
         model = Member #adding Member to model
         fields = ['id', 'full_name', 'phone', 'plan', 'fee_amount', 'fee_paid', 'status', 'due_date'] #Now seperating the Member values in fields.
+
+
